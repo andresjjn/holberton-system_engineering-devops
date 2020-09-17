60b7b044ffa7bb97411cc535020bcb0e0f713630
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-17 20:23:55 +0000

    Fix: 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,16 +6,10 @@ import json
 
 def number_of_subscribers(subreddit):
     """Funtion that return the number of subscribers of a subreddit"""
-    headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-                            AppleWebKit/537.36 (KHTML, like Gecko)\
-                            Chrome/70.0.3538.77 Safari/537.36"
-                    }
-    url = 'https://www.reddit.com/' + '/r/' + subreddit + '/about.json'
-    request = requests.get(url,
-                           headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+    request = requests.get(url, headers=headers, allow_redirects=False)
+    
     if request.status_code == 200:
-        request = json.loads(request.text)
-        result = request.get('data', {}).get('subscribers')
-        return result
+        return json.loads(request.text)['data']['subscribers']
     return 0
