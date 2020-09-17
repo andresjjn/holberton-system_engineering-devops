ead835c95ecadb7f519bc62e78af7231828979f7
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-17 20:24:39 +0000

    Fix: 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
     request = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if request.status_code == 200:
         return json.loads(request.text)['data']['subscribers']
     return 0
