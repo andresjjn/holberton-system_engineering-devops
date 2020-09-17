b410f8e5b98adc3f6d5a37c08406d5b5b54b2b72
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-17 21:24:29 +0000

    Add: 1-top_ten.py and 1-main.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This file contain the function top_ten"""
+import requests
+
+
+def top_ten(subreddit):
+    """Funtion that return the top ten of a subreddit"""
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json?limit=10'
+    request = requests.get(url, headers=headers, allow_redirects=False)
+
+    if request.status_code == 200:
+        dic = request.json()['data']['children']
+        for i in range(0, 10):
+            print(dic[i]['data']['title'])
+    return 0
+
+if __name__ == "__main__":
+    top_ten('programing')
