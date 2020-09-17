bf037d4723a8c973d1754a1abaa6ed602abedd56
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-17 20:02:32 +0000

    Fix: status_code in 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,10 +11,12 @@ def number_of_subscribers(subreddit):
                             AppleWebKit/537.36 (KHTML, like Gecko)\
                             Chrome/70.0.3538.77 Safari/537.36"
                     }
-    url = 'https://www.reddit.com/'
-    request = json.loads(requests.get(url + '/r/' + subreddit + '/about.json',
-                         headers=headers, allow_redirects=False).text)
-    result = request.get('data', {}).get('subscribers')
-    if result is None:
-        return 0
-    return result
+    url = 'https://www.reddit.com/' + '/r/' + subreddit + '/about.json'
+    request = requests.get(url,
+                         headers=headers, allow_redirects=False)
+    if request.status_code == 200:
+        request = json.loads(request.text)
+        result = request.get('data', {}).get('subscribers')
+        if result is not None:
+            return result
+    return 0
